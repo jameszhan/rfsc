9d597b476e7d066a9c5be8540f49a1bf88d26af4
James Zhan <zhiqiangzhan@gmail.com>
2014-05-08 20:43:14 +0800

    add rfsc clojure.

---
+++ clojure/src/main/java/com/alibaba/rfsc/clojure/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 1999-2004 Alibaba.com All right reserved. This software is the confidential and proprietary information of
+ * Alibaba.com ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with Alibaba.com.
+ */
+package com.alibaba.rfsc.clojure;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author zizhi.zhzzh
+ *         Date: 5/8/14
+ *         Time: 8:34 PM
+ */
+public class Hello {
+}
