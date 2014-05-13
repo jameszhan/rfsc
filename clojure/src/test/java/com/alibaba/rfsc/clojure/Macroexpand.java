8b39c1de77ce2580e2c41f6dc45bbb8470339232
James Zhan <zhiqiangzhan@gmail.com>
2014-05-14 01:22:02 +0800

    add macroexpand test

---
+++ clojure/src/test/java/com/alibaba/rfsc/clojure/Macroexpand.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 1999-2004 Alibaba.com All right reserved. This software is the confidential and proprietary information of
+ * Alibaba.com ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with Alibaba.com.
+ */
+package com.alibaba.rfsc.clojure;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author zizhi.zhzzh
+ *         Date: 5/14/14
+ *         Time: 12:24 AM
+ */
+public class Macroexpand {
+}
