a88fa0d7d2069e082bd4f01707354e5583550b4e
James Zhan <zhiqiangzhan@gmail.com>
2015-06-15 00:21:52 +0800

    temp fix.

---
+++ kickoff/src/main/java/com/mulberry/kickoff/Main.java
@@ -0,0 +1,16 @@
+package com.mulberry.kickoff;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author zizhi.zhzzh
+ *         Date: 1/19/15
+ *         Time: 2:13 PM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String s = "馬橋詞典.azw白崇禧先生訪問紀錄(上冊).azw心理催眠术：如何成为一名专业心理催眠师.azw希区柯克短篇故事集.azw浪漫主义的根源.azw心是孤独的猎手.azw文化树：世界文化简史.azw中式英语之鉴.azw正义：一场思辨之旅.azw论平等.azw给理想一点时间Ⅱ.azw旧制度与大革命.azw黑色大丽花.azw智慧政府：大数据治国时代的来临.azw关于国家的哲学理论.azw長恨歌.azw妖镜.azw浩荡两千年：中国企业公元前7世纪-1869年.azw不景气 不气馁：20位中外企业家逆境中的生存智慧.azw创新与企业家精神(珍藏版).azw财富是如何创造的.azw思想录.azw古船.azw旋風.azw公关专家不告诉你的“人际厚黑学”.azw自制力.azw编程之美：微软技术面试心得.azw苏东坡传.azw价格理论.azw简单武侠.azw傳奇.azw天行健7：旭日如血.azw西潮.azw杂的文.azw文化苦旅.azw金融炼金术.azw科学研究纲领方法论.azw哲学家们都干了些什么?.azw球状闪电.azw李鴻章.azw边际效用学派的兴起.azw蓝海战略.azw最后的贵族.azw圈子对了，事就成了.azw发现自由意志与个人责任.azw火与冰.azw半生緣.azw肉蒲团.azw长尾理论.azw这年头一定要会点经济学.azw回顾.azw疯癫与文明.azw失乐园.azw莫泊桑短篇小说精选集.azw挪威的森林.azw恶童日记三部曲.azw站在两个世界的边缘.azw王晋康科幻小说精选集1：养蜂人.azw如丧：我们终于老得可以谈谈未来.azw立法者与阐释者.azw全世界聪明人都在学的70个思维方式.azw为什么你的公司没长大.azw致加西亚的信.azw科学与社会秩序.azw家.azw好妈妈胜过好老师.azw向万科学管理.azw会说话是一种资本：当众讲话的艺术.azw消失中的江城.azw财产权利与制度变迁.azw缔造苹果神话：乔布斯传.azw超级魔术记忆法：记得快•记得准•记得牢.azw艺术哲学.azw国史新论.azw枪炮、病菌与钢铁.azw懂管理先要懂心理.azw日本四大推理奇书之黑死馆杀人事件.azw大数据在中国.azw大象的眼泪.azw性爱圣经.azw山中少年今何在.azw第三波：20世纪后期民主化浪潮.azw经济学方法论.azw霍乱时期的爱情.azw圈子圈套3.azw通往奴役之路.azw双面催眠师.azw如何阅读一本书.azw别为小事抓狂.azw官僚体制的政治(制度经济学译丛).azw伟大是熬出来的：冯仑与年轻人闲话人生(黑天鹅).azw国家兴衰探源.azw惊人的假说.azw爱你就像爱生命.azw论人类不平等的起源和基础.azw改革历程.azw基业长青.azw瀕死之眼.azw历史是个什么玩意儿1.azw与爱因斯坦月球漫步：美国记忆力冠军教你记忆一切.azw中国官僚政治研究.azw寂静的春天.azw诸神的传说：希腊神话故事.azw孽子BY.azw策略思维.azw天才在左疯子在右.azw替天行道：王晋康科幻小说精选集2.azw财务禁书.azw古代法.azw经济发展理论.azw东藏记.azw你应该知道的2000个健康细节.azw这些年，马云犯过的错误.azw每晚一个离奇故事之惊悚夜.azw第五项修炼:学习型组织的艺术与实践.azw你其实不懂法律纠纷课.azw十亿消费者.azw纳兰词笺注.azw吴敬琏传.azw狂人日记.azw克苏鲁神话Ⅱ：恐怖人间.azw唐诗三百首.azw52种行之有效的减压方法.azw基本无害.azw受戒.azw狗日的糧食.azw惹事.azw原鄉人.azw1989：见证最后的美丽.azw倪匡說鬼.azw嫁妝一牛車.azw荷花澱.azw公墓.azw將軍底頭.azw組織部來了個年輕人.azw莎乐美.azw不畏将来不念过去.azw四喜憂國.azw莎菲女士的日記.azw將軍族.azw六祖坛经.azw棋王.azw賴索.azw小二黑結婚.azw兒子的大玩偶.azw我的孤独是一座花园.azw兔子屋的秘密.azw共产党宣言.azw郭德纲：过得刚好.azw孤独是你的必修课.azw有味.azw哲学的慰藉.azw欧亨利短篇小说集.azw誰搬走了我的乳酪.azw岡底斯的誘惑.azw城门开.azw岁月的泡沫.azw格列佛游记.azw小时候就在想的事.azw盔甲骑士.azw最初的爱情,最后的仪式.azw我与狗狗的十个约定.azw老人與海.azw尘曲完整版.azw安房直子系列的附录.azw天坑·鹰猎.azw沉淪.azw在星巴克要买大杯咖啡.azw直到花豆煮熟：小夜的故事.azw温故一九四二.azwExcel函数辞典.azw我城.azw起风了(宫崎骏收山之作原著小说).azw邊城.azw当世界年纪还小的时候.azw公考的那些日子.azw有闲阶级论.azw“灵魂之战”第二部：陨星之巨龙.azw规训与惩罚.azw暴风雨的记忆.azw一地鸡毛.azw青春.azw情商：为什么情商比智商更重要.azw书读完了.azw静静的顿河.azw台灣現代化的推手：蔣經國傳.azw城管来了.azw文明的冲突与世界秩序的重建.azw荣氏真相.azw政府论.azw李嘉诚管理日志.azw《史记》早该这样读.azw阿桑奇自传：不能不说的秘密.azw万法简史.azw灵山.azw品人录.azw把话说到领导心坎里.azw语言文学专业学术论文写作导引.azw女性主义.azw生命中最重要的.azw博尔赫斯小说集.azw大败局Ⅰ.azw精灵宝钻.azw沉重的翅膀.azw男人的一半是女人.azw一个人的朝圣.azw管理与销售的10大手段.azw查太莱夫人的情人.azw嫌疑人X的献身.azw人的条件.azw终极爆炸：王晋康科幻小说精选集3.azw英儿.azw少有人走的路.azw围城.azw心理控制术.azw定西孤儿院纪事.azw自卑与超越.azw水晶头骨之谜.azw这绝不仅仅是客套话.azw故乡天下黄花.azw现代经理人不可不知的法律知识.azw观念的水位.azw賭徒陳湯.azw李嘉诚全传.azw圈子圈套2.azw面具馆.azw扫起落叶好过冬.azw数据挖掘技术与工程实践.azw五天学会绘画.azw蚂蚁三部曲一：蚂蚁.azw深入理解Android：卷I.azw代码的未来.azw自然计算：DNA、量子比特和智能机器的未来.azw深入理解Android：卷II.azw奇妙的医学知识.azw资治通鉴.azw3D打印：从想象到现实.azw三国演义(绣像版).azw一路走来一路读.pdf别做正常的傻瓜.azw思考的乐趣：Matrix67数学笔记.azw谷歌吴军：数学之美.azwThe Swift Programming Language(中文版).azwCPU架构10年.azw3OnLisp.azw3";
+
+    }
+}
